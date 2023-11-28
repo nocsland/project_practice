a983833de56ffc1b527fc2c122d3c484c5364e94
Anton Vlasov <nocsland@gmail.com>
2023-11-28 11:24:59 +0400

    added students_helper_m_gpt

---
+++ PP1/students_helper_m_gpt/students_helper_m_gpt.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import pipeline
+
+model_name = "ai-forever/mGPT"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+
+# Декоратор @st.cache говорит Streamlit, что модель нужно загрузить только один раз, чтобы избежать утечек памяти
+@st.cache_resource
+# загружает модель
+def load_model():
+    return pipeline("text-generation", model=model, tokenizer=tokenizer)
+
+
+# Загружаем предварительно обученную модель
+answer = load_model()
+
+# Выводим заголовок страницы
+st.title("Помощник студента")
+
+# Получаем текст для анализа
+text = st.text_area("Введите запрос")
+
+# Создаем кнопку
+button = st.button('Генерировать')
+
+# Выводим результат по нажатию кнопки
+if button:
+    st.subheader("Вот мой ответ:")
+    st.write(answer(text))
