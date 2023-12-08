a344f5e9eb7bb26df5cd1f715a4a3520ee37833d
Anton Vlasov <nocsland@gmail.com>
2023-12-08 10:38:40 +0400

    updated run.sh

--- PP1/summary_text/run.sh
+++ PP1/summary_text/run.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-# Проверяем, активировано ли виртуальное окружение
+# Скрипт проверит активировано ли виртуальное окружение, в случае необходимости создаст его и установит зависимости
+# Запускать из корневого каталога проекта командой ./PP1/summary_text/run.sh
+
+# Проверяем, находимся ли мы в корневом каталоге проекта
+if [[ "$(basename "$(pwd)")" != "project_practice" ]]; then
+  echo "Скрипт должен быть запущен из корневого каталога проекта"
+  exit 1
+fi
+
+# Проверяю, активировано ли виртуальное окружение
 if [[ -z "${VIRTUAL_ENV}" ]]; then
    echo "Виртуальное окружение не активировано"
 
-   echo "Проверяю и создаю виртуальное окружение"
+   # "Проверяю и создаю виртуальное окружение"
    python3 -m venv ~/.virtualenvs/project_practice/
 
-   echo "Активирую виртуальное окружение"
+   # "Активирую виртуальное окружение"
    source "$HOME"/.virtualenvs/project_practice/bin/activate
 
    echo "Проверяю и устанавливаю зависимости"
-   pip install -r src/requirements.txt
+   pip install -r PP1/summary_text/src/requirements.txt
 else
    echo "Виртуальное окружение уже активировано"
 fi
-echo "Запускаю приложение"
-cd src
+# "Запускаю приложение"
+cd PP1/summary_text/src || exit
 streamlit run summary_text.py
